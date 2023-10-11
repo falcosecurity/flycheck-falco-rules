4bdc576abb13569354281badeaafe4abeee7fb3d
Leonardo Grasso <me@leonardograsso.com>
2023-10-11 13:02:11 +0200

    docs: add spdx id
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- flycheck-falco-rules.el
+++ flycheck-falco-rules.el
@@ -1,6 +1,7 @@
 ;;; flycheck-falco-rules.el --- On-the-fly syntax checking for falco rules files -*- lexical-binding: t; -*-
+;; SPDX-License-Identifier: Apache-2.0
 ;;
-;; Copyright (C) 2022 The Falco Authors.
+;; Copyright (C) 2023 The Falco Authors.
 ;;
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
